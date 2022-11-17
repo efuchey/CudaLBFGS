f3147d2d03baa1960ef71c9bdfb61e484729f45c
fermipdoc <efuchey@gmail.com>
2022-11-17 12:56:34 -0600

    Removed include of "device_functions.h" from the "projects" files.

--- projects/sqtrack_chi2/sqtrack_chi2.cu
+++ projects/sqtrack_chi2/sqtrack_chi2.cu
@@ -11,10 +11,10 @@
 
 using namespace std;
 
-class cpu_parabola : public cpu_cost_function
+class cpu_sqtrack_chi2 : public cpu_cost_function
 {
 public:
-	cpu_parabola(float a, float b, float c) 
+	cpu_sqtrack_chi2(float a, float b, float c) 
 	: cpu_cost_function(1)
 	, m_a(a)
 	, m_b(b)
@@ -39,16 +39,58 @@ public:
 	}
 	
 private:
-	float m_a;
-	float m_b;
-	float m_c;
+	float m_x0;
+	float m_y0;
+	float m_tx;
+	float m_ty;
+	float m_pinv;
 };
 
-namespace gpu_parabola_d
+namespace gpu_sqtrack_chi2_d
 {
-	__device__ float m_a;
-	__device__ float m_b;
-	__device__ float m_c;
+	__device__ float m_x0;
+	__device__ float m_y0;
+	__device__ float m_tx;
+	__device__ float m_ty;
+	__device__ float m_pinv;
+
+	__global__ void kernel_combined_f_gradf(const float *d_drift, const float *d_res, 
+		const float *d_p1x, const float *d_p1y, const float *d_p1z, 
+		const float *d_deltapx, const float *d_deltapy, const float *d_deltapz, 
+		float *d_chi2, float *d_grad)
+	{
+
+		const float &Den = sqrtf( *d_deltapy * *d_deltapy +  *d_deltapx * *d_deltapx + 2.0f * *d_deltapx * *d_deltapy * m_tx * m_ty );
+		const float &dca = ( -1.0f * *d_deltapy * ( d_p1x - m_x0 ) + *d_deltapx * ( d_p1y - m_y0 ) - *d_p1z ( m_tx * *d_deltapy- m_ty * *d_deltapy )  ) / Den;
+		const float &value = ( *d_drift - dca ) * ( *d_drift - dca ) / *d_res / *d_res;
+		*d_chi2 = value;
+/*
+	for(int i = 0; i<n_points; i++){
+		dca = ( -deltapy[i]*(p1x[i]-output_parameters[0]) + deltapx[i]*(p1y[i]-output_parameters[1]) + p1z[i]*(output_parameters[2]*deltapy[i]-output_parameters[3]*deltapx[i]) ) / sqrtf( deltapy[i]*deltapy[i] + deltapx[i]*deltapx[i] - 2*output_parameters[2]*output_parameters[3]*deltapy[i]*deltapx[i] );
+		values[i] = (driftdist[i] - dca) * (driftdist[i] - dca) / resolutions[i] / resolutions[i];
+		chi2+= values[i];
+
+	     	Den2 = deltapy[i]*deltapy[i] + deltapx[i]*deltapx[i] - 2 * ( deltapx[i]*deltapy[i]*output_parameters[2]*output_parameters[3]);
+	     	Den = sqrtf(Den2);
+		
+		//dchi2/dx0:
+		derivatives[0*n_points+i] = -2*values[i]*deltapy[i]/Den;
+		//dchi2/dy0:
+		derivatives[1*n_points+i] = +2*values[i]*deltapx[i]/Den;
+		//dchi2/dtx:
+		derivatives[2*n_points+i] = -2*values[i]*(deltapy[i]*p1z[i]/Den-dca*output_parameters[3]*deltapx[i]*deltapy[i]/Den2);
+		//dchi2/dty:
+		derivatives[3*n_points+i] = +2*values[i]*(deltapx[i]*p1z[i]/Den-dca*output_parameters[2]*deltapx[i]*deltapy[i]/Den2);
+
+		*d_chi2 = 2.0f * m_a * x + m_b;
+	}
+*/
+	__global__ void kernelGradf(const float *d_x, float *d_y, float *d_grad)
+	{
+		const float &x = *d_x;
+		*d_y = m_a * x * x + m_b * x + m_c;
+		*d_grad = 2.0f * m_a * x + m_b;
+	}
 
 	__global__ void kernel_combined_f_gradf(const float *d_x, float *d_y, float *d_grad)
 	{
@@ -58,53 +100,60 @@ namespace gpu_parabola_d
 	}
 }
 
-class gpu_parabola : public cost_function
+class gpu_sqtrack_chi2 : public cost_function
 {
 public:
-	gpu_parabola(float a, float b, float c) 
+	gpu_sqtrack_chi2(float a, float b, float c) 
 	: cost_function(1) 
 	{
-		CudaSafeCall( cudaGetSymbolAddress((void**)&m_d_a, gpu_parabola_d::m_a) );
-		CudaSafeCall( cudaGetSymbolAddress((void**)&m_d_b, gpu_parabola_d::m_b) );
-		CudaSafeCall( cudaGetSymbolAddress((void**)&m_d_c, gpu_parabola_d::m_c) );
+		CudaSafeCall( cudaGetSymbolAddress((void**)&m_d_x0, gpu_sqtrack_chi2_d::m_x0) );
+		CudaSafeCall( cudaGetSymbolAddress((void**)&m_d_y0, gpu_sqtrack_chi2_d::m_y0) );
+		CudaSafeCall( cudaGetSymbolAddress((void**)&m_d_tx, gpu_sqtrack_chi2_d::m_tx) );
+		CudaSafeCall( cudaGetSymbolAddress((void**)&m_d_ty, gpu_sqtrack_chi2_d::m_tx) );
+		CudaSafeCall( cudaGetSymbolAddress((void**)&m_d_pinv, gpu_sqtrack_chi2_d::m_pinv) );
 
-		CudaSafeCall( cudaMemcpy(m_d_a, &a, sizeof(float), cudaMemcpyHostToDevice) );
-		CudaSafeCall( cudaMemcpy(m_d_b, &b, sizeof(float), cudaMemcpyHostToDevice) );
-		CudaSafeCall( cudaMemcpy(m_d_c, &c, sizeof(float), cudaMemcpyHostToDevice) );
+		CudaSafeCall( cudaMemcpy(m_d_x0, &x0, sizeof(float), cudaMemcpyHostToDevice) );
+		CudaSafeCall( cudaMemcpy(m_d_y0, &y0, sizeof(float), cudaMemcpyHostToDevice) );
+		CudaSafeCall( cudaMemcpy(m_d_tx, &tx, sizeof(float), cudaMemcpyHostToDevice) );
+		CudaSafeCall( cudaMemcpy(m_d_ty, &ty, sizeof(float), cudaMemcpyHostToDevice) );
+		CudaSafeCall( cudaMemcpy(m_d_pinv, &pinv, sizeof(float), cudaMemcpyHostToDevice) );
 	}
 	
 	void f_gradf(const float *d_x, float *d_f, float *d_grad)
 	{
-		gpu_parabola_d::kernel_combined_f_gradf<<<1, 1>>>(d_x, d_f, d_grad);
+		gpu_sqtrack_chi2_d::kernel_combined_f_gradf<<<1, 1>>>(d_x, d_f, d_grad);
 		CudaCheckError();
 	}
 	
 private:
 
-	float *m_d_a; 
-	float *m_d_b;
-	float *m_d_c;
+	float *m_d_x0; 
+	float *m_d_y0;
+	float *m_d_tx;
+	float *m_d_ty;
+	float *m_d_pinv;
 };
 
+/*
 int main(int argc, char **argv)
 {
 	// CPU
 
-	cpu_parabola p1(4.0f, 2.0f, 6.0f);
+	cpu_sqtrack_chi2 p1(4.0f, 2.0f, 6.0f);
 	lbfgs minimizer1(p1);
 
 	float x = 8.0f;
 	{
-		timer t("parabola_cpu");
+		timer t("sqtrack_chi2_cpu");
 		t.start();
 		minimizer1.minimize_with_host_x(&x);
 	}
 	
-	cout << "CPU Parabola: " << x << endl;
+	cout << "CPU sqtrack_Chi2: " << x << endl;
 
 	// GPU
 
-	gpu_parabola p2(4.0f, 2.0f, 6.0f);
+	gpu_sqtrack_chi2 p2(4.0f, 2.0f, 6.0f);
 	lbfgs minimizer2(p2);
 	
 	x = 8.0f;
@@ -114,7 +163,7 @@ int main(int argc, char **argv)
 	CudaSafeCall( cudaMemcpy(d_x, &x, sizeof(float), cudaMemcpyHostToDevice) );
 
 	{
-		timer t("parabola_gpu");
+		timer t("sqtrack_chi2_gpu");
 		t.start();
 		minimizer2.minimize(d_x);
 	}
@@ -122,7 +171,8 @@ int main(int argc, char **argv)
 	CudaSafeCall( cudaMemcpy(&x, d_x, sizeof(float), cudaMemcpyDeviceToHost) );
 	CudaSafeCall( cudaFree(d_x) );
 
-	cout << "GPU Parabola: " << x << endl;
+	cout << "GPU sqtrack_Chi2: " << x << endl;
 
 	return 0;
 }
+*/
